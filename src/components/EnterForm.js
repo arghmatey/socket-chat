a63b63d1de675887fd971c167691b73d84a04e54
Sarah <40401149+arghmatey@users.noreply.github.com>
2024-03-04 17:43:48 -0800

    Refactor entry form logic

--- src/components/EnterForm.js
+++ src/components/EnterForm.js
@@ -1,26 +1,48 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { redirect } from 'react-router-dom';
 
 const EnterForm = () => {
     const [username, setUsername] = useState('');
     const [room, setRoom] = useState('');
 
+    const handleUsernameChange = e => {
+        setUsername(e.target.value);
+    }
+
+    const handleRoomChange = e => {
+        setRoom(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (room && username) {
+            redirect(`/chatroom?username=${username}&room=${room}`);
+        }
+    }
+
     return (
         <div className="formWrapper">
             <h1>Enter Room</h1>
-            <input placeholder="Username" className="formInput" type="text" onChange={e => setUsername(e.target.value)} />
-            <select placeholder="Select a Room" className="formInput" onChange={e => setRoom(e.target.value)}>
-                <option disabled="" default>Select a Room</option>
-                <option>Room 1</option>
-                <option>Room 2</option>
-                <option>Room 3</option>
-            </select>
-            <Link
-                onClick={e => (!username || !room) ? e.preventDefault() : null}
-                to={`/chatroom?username=${username}&room=${room}`}
-            >
-                <button className="formButton">Enter</button>
-            </Link>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        placeholder="Username"
+                        className="formInput"
+                        type="text"
+                        onChange={handleUsernameChange}
+                    />
+                    <select
+                        placeholder="Select a Room"
+                        className="formInput"
+                        onChange={handleRoomChange}
+                    >
+                        <option disabled default>Select a Room</option>
+                        <option>Room 1</option>
+                        <option>Room 2</option>
+                        <option>Room 3</option>
+                    </select>
+                    <button className="formButton">Enter Room</button>
+            </form>
         </div>
     )
 }
